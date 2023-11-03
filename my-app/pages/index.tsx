a38705561063afafe66fb0c072a5d42482ff3fb7
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-03 11:31:29 +0000

    home page ui update

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,25 +1,35 @@
-import { Box, Button, Center, Img, Input, InputGroup, InputLeftAddon, Text, VStack } from "@chakra-ui/react"
+import { Box, Button, Center, Img, Input, InputGroup, InputLeftAddon, Text, VStack, Divider, cookieStorageManager } from "@chakra-ui/react"
+import { useState } from "react"
 
 
 export default function Index() {
-  console.log("log")
+  const [name, setName] = useState()
+
+const handleSubmit=()=>{
+  
+}
+
+
   return (
-    
-      <Center w={"100%"} h={"100vh"}>
-        <VStack>
-          <Text fontSize='6xl'>Login</Text>
-      <Box w={"600px"} h={"400px"} boxShadow='dark-lg' rounded='xl'>
-        <VStack>
-        <Img src="../man.svg" w={"200px"}/>
-        <InputGroup display={"flex"} justifyContent={"center"} mt={"20px"}>
-          <InputLeftAddon children='Name' />
-          <Input type='text' placeholder='Enter Your Name' w={"300px"}/>
-        </InputGroup>
-        <Button mt={"20px"}>Click Here</Button>
-        </VStack>
-      </Box>
+
+    <Center w={"100%"} h={"100vh"}>
+      <VStack>
+        <Text fontSize='6xl'>Login</Text>
+        <Divider w="200px" h="10px" bgColor={"blue.400"} />
+        <Box w={"600px"} h={"400px"} boxShadow='dark-lg' rounded='xl'>
+          <VStack>
+            <Img src="../man.svg" w={"200px"} />
+            <InputGroup display={"flex"} justifyContent={"center"} mt={"20px"}>
+              <InputLeftAddon children='Name' />
+              <Input type='text' value={name} placeholder='Enter Your Name' w={"300px"} onChange={(e)=>setName(e.target.value)} />
+            </InputGroup>
+            <Button colorScheme='teal' variant='solid' mt="50px" w="200px" onClick={()=>handleSubmit}>
+             Register
+            </Button>
+          </VStack>
+        </Box>
       </VStack>
-      </Center>
-   
+    </Center>
+
   )
 }
