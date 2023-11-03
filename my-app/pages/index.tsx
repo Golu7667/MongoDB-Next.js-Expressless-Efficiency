311ec5a0b9e0dbc2095a058f97a65a8dc2612956
Golu Choudhary <103061012+Golu7667@users.noreply.github.com>
2023-11-03 11:12:28 +0000

    home page ui done

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,9 +1,25 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Center, Img, Input, InputGroup, InputLeftAddon, Text, VStack } from "@chakra-ui/react"
 
 
 export default function Index() {
-    console.log("log")
-      return (
-        <Box m={2} bgColor={"green.100"}>Tomato</Box>
-      )
-    }
+  console.log("log")
+  return (
+    
+      <Center w={"100%"} h={"100vh"}>
+        <VStack>
+          <Text fontSize='6xl'>Login</Text>
+      <Box w={"600px"} h={"400px"} boxShadow='dark-lg' rounded='xl'>
+        <VStack>
+        <Img src="../man.svg" w={"200px"}/>
+        <InputGroup display={"flex"} justifyContent={"center"} mt={"20px"}>
+          <InputLeftAddon children='Name' />
+          <Input type='text' placeholder='Enter Your Name' w={"300px"}/>
+        </InputGroup>
+        <Button mt={"20px"}>Click Here</Button>
+        </VStack>
+      </Box>
+      </VStack>
+      </Center>
+   
+  )
+}
